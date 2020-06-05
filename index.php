f3f73e016d882e1cd2a0be46d99ba40155e0ec02
Fabian <19959@ma-web.nl>
2020-06-05 12:22:28 +0200

    af tot en met video twee volgende keer verder vanaf video 3

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php if (have_posts()):?>
-    <?php while (have_posts()): the_post();?>
-        // zaken met de posts
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <!-- zaken met de posts -->
+        <h3><?php the_title() ?></h3>
+        <div><?php the_content() ?></div>
     <?php endwhile ?>
-    <?php else: ?>
+<?php else : ?>
+
 <?php endif ?>
 
-
-
-
+<!-- TODO verder vanaf video 3: https://blanken5.home.xs4all.nl/WP_themaMaken.html# -->
