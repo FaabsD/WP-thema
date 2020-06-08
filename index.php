ba0dfb33895aec1633c1ebb6adc82f13e087456a
Fabian <19959@ma-web.nl>
2020-06-08 16:13:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <!-- zaken met de posts -->
-        <h3><?php the_title() ?></h3>
-        <div><?php the_content() ?></div>
-    <?php endwhile ?>
-<?php else : ?>
+<?php get_header()?>
 
-<?php endif ?>
 
+    <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <!-- zaken met de posts -->
+            <h3><?php the_title() ?></h3>
+            <div><?php the_content() ?></div>
+        <?php endwhile ?>
+    <?php else : ?>
+        <p>Geen berichten gevonden</p>
+    <?php endif ?>
+<?php get_footer()?>
 <!-- TODO verder vanaf video 3: https://blanken5.home.xs4all.nl/WP_themaMaken.html# -->
