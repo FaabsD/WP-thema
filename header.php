28461a4d70115872a46514f2fd639b634aca8f2b
Fabian <19959@ma-web.nl>
2020-06-08 16:24:25 +0200

    aanpassing aan header author = hardcoded

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="<?php bloginfo('author')?>">
+    <meta name="author" content="Fabian Hendriks">
     <title><?php bloginfo('name')?></title>
 </head>
 
