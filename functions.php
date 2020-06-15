761fea70d44e3c1087fd010df0382b76d96b19b7
Fabian <19959@ma-web.nl>
2020-06-15 16:30:35 +0200

    roep functie aan voor wp-nav-walker

--- functions.php
+++ functions.php
@@ -1,18 +1,28 @@
 <?php
 // laad stylesheets
-function loadStylesheet(){
+function loadStylesheet()
+{
     wp_enqueue_style('style', get_stylesheet_uri());
-    wp_enqueue_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
 }
 add_action('init', 'loadStylesheet');
 
 // menu registreren
 
-function registerMenu(){
+function registerMenu()
+{
     $argumenten = array(
         'hoofd-menu' => __('Hoofd menu')
     );
     register_nav_menus();
 }
 add_action('init', 'registerMenu');
+
+/**
+ * Register Custom Navigation Walker
+*/
+function register_navwalker(){
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
 ?>
