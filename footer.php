a3f5aabd593f410739c73299b4e5a62496487429
Fabian <19959@ma-web.nl>
2020-06-19 11:43:43 +0200

    kleine fixes

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
             &copy; <?php the_author()?> 
             <small><?php echo date('Y')?></small>
         </footer>
-        <?php wp_footer()?>
+        <?php wp_footer();?>
     </body>
 </html>
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Fabian Hendriks">
     <title><?php bloginfo('name') ?></title>
-    <?php wp_head() ?>
+    <?php wp_head(); ?>
 </head>
 
 <body>
