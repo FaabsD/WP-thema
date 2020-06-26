5e44ffd3349d7c1edc18a46cead4ad7d3aa26f0d
Fabian <19959@ma-web.nl>
2020-06-26 11:58:19 +0200

    widget in footer toegevoegd

--- footer.php
+++ footer.php
@@ -1,7 +1,14 @@
         <footer class="bg-primary text-white">
             <div class="container">
-                &copy; <?php the_author()?> 
-                <small><?php echo date('Y')?></small>
+                <div class="row">
+                    <div class="col-md-4">
+                        &copy; <?php the_author()?> 
+                        <small><?php echo date('Y')?></small>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <?php dynamic_sidebar('under')?>
+                </div>
             </div>
         </footer>
         <?php wp_footer();?>
